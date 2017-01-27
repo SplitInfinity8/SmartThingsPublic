cd5f8c460f5170ad6f0bb5cc75de5cb9a4fb66c4
SplitInfinity8 <jay@darkroad.com>
2017-01-27 12:19:18 -0600

    Update smart-auto-lock-alert.groovy
    Added reference note.

--- smartapps/smart-auto-lock-unlock/smart-auto-lock-unlock.src/smart-auto-lock-alert.groovy
+++ smartapps/smart-auto-lock-unlock/smart-auto-lock-unlock.src/smart-auto-lock-alert.groovy
@@ -12,7 +12,9 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
-
+ *
+ * Notes on state storage: http://docs.smartthings.com/en/latest/smartapp-developers-guide/state.html
+ *
 *** Workflow layout ***
 Auto-lock after __ minutes: Yes/No (Default 15, Yes)
   -(Alert on auto-lock attempt)
